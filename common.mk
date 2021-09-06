c623f001928ccfffa5510b1b903eea9f41bd081d
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2021-09-06 16:11:09 +0200

    sm7250-common: rootdir: Remove unused FTM init scripts
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- common.mk
+++ common.mk
@@ -204,12 +204,10 @@ PRODUCT_PACKAGES += \
 
 # Common init scripts
 PRODUCT_PACKAGES += \
-    ftm_power_config.sh \
     init.class_main.sh \
     init.cust.rc \
     init.oem.debug.rc \
     init.oem.rc \
-    init.oem_ftm.rc \
     init.oplus_chg.sh \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
@@ -224,9 +222,7 @@ PRODUCT_PACKAGES += \
     init.qti.qcv.sh \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc \
-    vendor.oem_ftm.rc \
-    vendor.oem_ftm_svc_disable.rc
+    ueventd.qcom.rc
 
 # Component overrides
 PRODUCT_COPY_FILES += \
